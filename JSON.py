366e6edac60245eae7d5f2c11d0eb1ad1cdf9f92
mudouasenha <30910244+mudouasenha@users.noreply.github.com>
2017-09-18 22:36:06 -0300

    Add files via upload

---
+++ JSON.py
@@ -0,0 +1,52 @@
+var jason = {
+    "idade" : "25",
+    "casa" : "japao",
+    "genero": "fluid"
+};
+#cria o objeto jason que pode ser acessado 
+
+print(jason.idade)
+
+
+
+
+{
+    rota1 = {
+        "ID": {
+            "ID1": "1",
+            "ID2": "2",
+            "ID3": "3",
+            "ID4": "4",
+            "ID5": "5"
+        },
+        "Função": {
+            "ligar": "l",
+            "frente": "f",
+            "direita": "d",
+            "esquerda": "e",
+            "para": "p",
+            "socorro": "s",
+            "trabalhando": "t",
+            "online": "o"
+        }
+    },
+    rota2 = {
+        "ID": {
+            "ID6": "6",
+            "ID7": "7",
+            "ID8": "8",
+            "ID9": "9",
+            "ID10": "10"
+        },
+        "Função": {
+            "ligar": "l",
+            "frente": "f",
+            "direita": "d",
+            "esquerda": "e",
+            "para": "p",
+            "socorro": "s",
+            "trabalhando": "t",
+            "online": "o"
+        }
+    }
+};
