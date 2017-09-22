ea8631b68b924f1292980e4fc6e9aab00d667774
mudouasenha <mudouasenha@gmail.com>
2017-09-22 15:00:53 -0400

    codeanywhere arquivos mqttprojeto

---
+++ mqtttestes.py
@@ -0,0 +1,43 @@
+import json
+
+
+julinho = {
+    rota1 = {
+        "ID": {
+            "ID1": "1",
+            "ID2": "2",
+            "ID3": "3",
+            "ID4": "4",
+            "ID5": "5"
+        },
+        "Função": {
+            "ligar": "l",
+            "frente": "f",
+            "direita": "d",
+            "esquerda": "e",
+            "para": "p",
+            "socorro": "s",
+            "trabalhando": "t",
+            "online": "o"
+        }
+    },
+    rota2 = {
+        "ID": {
+            "ID6": "6",
+            "ID7": "7",
+            "ID8": "8",
+            "ID9": "9",
+            "ID10": "10"
+        },
+        "Função": {
+            "ligar": "l",
+            "frente": "f",
+            "direita": "d",
+            "esquerda": "e",
+            "para": "p",
+            "socorro": "s",
+            "trabalhando": "t",
+            "online": "o"
+        }
+    }
+};
--- trabson.py
+++ trabson.py
@@ -1,31 +1,21 @@
 import paho.mqtt.client as mqtt
 
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("projetointegrador/julinho/#")
+    client.subscribe("julinho/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    for item in msg.topic.split('/'):
+      if item != '':
+        print(item)
     print(msg.payload)
-    rota = {}
-    rota['funcao1'] = [f]
-    rota['funcao2'] = [d]
-    rota['funcao3'] = [e]
-        def comandos(f, d, e):
-            print 
-    rota['rota1'] = ['funcao1', 'funcao2', 'funcao3']
-    rota['rota2'] = ['IDfuncao2, IDfuncao2, IDfuncao2']
-    if msg.payload == 'rota1':
-        print rota['rota1']
-    elif msg.payload =='rota2':
-        print rota['rota2']
-    else:
-        print 'inexistente'
-    
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -37,3 +27,10 @@ client.connect("iot.eclipse.org", 1883, 60)
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 client.loop_forever()
+
+
+
+
+def sos():
+  for item in
+  if msg.topic != 'julinho/sos'
